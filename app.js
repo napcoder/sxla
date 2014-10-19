011bebe7748eb2e092fa8c37ca96e703bf7f7f3b
Marco Travaglini <marcotravaglini81@gmail.com>
2014-10-20 00:32:41 +0200

    fixed some log bug, finished import program

--- app.js
+++ app.js
@@ -2,24 +2,32 @@
 'use strict';
 
 var winston = require('winston'),
+	path = require('path'),
+    mcapi = require('mailchimp-api'),
     Parser = require('./lib/parser'),
-    ApiWrapper = require('./lib/api-wrapper'),
-    mcapi = require('mailchimp-api');
+    ApiWrapper = require('./lib/api-wrapper');
+
+var date = new Date();
+date = date.toJSON().replace(/(-|:)/g, '.');
 
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, { level: 'silly'});
 winston.add(winston.transports.File, { 
-    filename: path.join('./', 'logs', 'sxla.log'),
+    filename: path.join('./', 'logs', 'sxla' + date + '.log'),
     level: 'silly',
     timestamp: true
 });
 
+winston.info('*********** APPLICATION STARTED ***********');
+
 var apiKey = process.env.MAILCHIMP_SXLA_API_KEY;
 var listId = process.env.MAILCHIMP_SXLA_LIST_ID;
-logger.debug('apiKey: ', apiKey);
-logger.debug('listId: ', listId);
+winston.debug('apiKey: ', apiKey);
+winston.debug('listId: ', listId);
 
 var api = new ApiWrapper(mcapi, apiKey, listId, winston);
 
 var parser = new Parser(winston);
-parser.parseCsv(__dirname + '/data/Soci14-15.csv');
+parser.parseCsv(__dirname + '/data/soci14-15.csv', function (data) {
+	api.batchSubscribe(data);
+});
